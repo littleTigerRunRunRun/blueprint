fc1a3815c459b6022e226ec1346e66028fc6ad9c
tiger <894416038@qq.com>
2025-02-20 13:14:43 +0800

    feat(port): auto port

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,39 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { fileURLToPath, URL } from 'node:url'
+import detectPort from 'detect-port'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      globals: {
-        Buffer: true,
-        global: true,
-        process: true
-      },
-      protocolImports: true
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(async () => {
+  const DEFAULT_PORT = 3000
+  const port = await detectPort(DEFAULT_PORT)
+
+  return {
+    server: {
+      port
+    },
+    plugins: [
+      react(),
+      nodePolyfills({
+        globals: {
+          Buffer: true,
+          global: true,
+          process: true
+        },
+        protocolImports: true
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      rollupOptions: {
+        external: ['react', 'react-dom', 'react-router-dom', 'antd']
+      }
+    },
+    optimizeDeps: {
+      exclude: []
     }
-  },
-  build: {
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react-router-dom', 'antd']
-    }
-  },
-  optimizeDeps: {
-    exclude: []
   }
 })
